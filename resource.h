da1cd115cb4484b00ac0ba02d352373d684fd9ff
Michael Guo <35714546+mguo8@users.noreply.github.com>
2020-01-19 18:16:18 -0500

    Add project files.

---
+++ resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by HRM.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_HRM_DIALOG                  102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define IDR_MAINMENU					130
+#define ID_MENUUSER						131
+#define ID_MENUPASSWORD					132
+#define ID_MENUEXIT						133
+#define ID_MENUDEPT						134
+#define ID_MENUPERSON					135
+#define ID_MEMUCHECK					136
+#define ID_MENUCHECKSUM					137
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
